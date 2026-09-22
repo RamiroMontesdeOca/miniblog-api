2edbe9862c057aea86617dcc6d8cadb8cc9e4a49
ramiro alejo <ramiroalejo1234@gmail.com>
2026-09-22 01:52:50 -0300

    se modifico el authors para que funcione directamente con el service

--- routes/authors.js
+++ routes/authors.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db/config');
+const { getAuthors,getAuthorsById,createAuthor,updateAuthor,deleteAuthor } = require('../services/authorsService');
 
 router.get('/', async(req, res)=>{
     try {
-        const result = await pool.query('SELECT * FROM authors ORDER BY nombre');
-        res.json(result.rows);
+        const authors = await getAuthors();
+        res.json(authors);
     }catch (error) {
         console.error('Error obteniendo autores', error);
         res.status(500).json({error:'Error obtiendio autores'});
@@ -14,13 +14,11 @@ router.get('/', async(req, res)=>{
 
 router.get('/:id', async(req, res)=>{
     try{
-        const result = await pool.query('SELECT * FROM authors WHERE id = $1', 
-            [req.params.id]
-        );
-        if (result.rows.length === 0){
+        const author = await getAuthorsById (req.params.id)
+        if (!author){
             return res.status(404).json({ error: 'autor no encontrado'});
         }
-        res.json(result.rows[0]);
+        res.json(author);
     }catch (error) {
         console.error('Error obteniendo autor', error);
         res.status(500).json({ error: 'Error obteniendo autor'});
@@ -33,11 +31,8 @@ router.post('/', async (req, res)=>{
         return res.status(400).json({error: 'Nombre y email son requeridos'});
     }
     try {
-        const result = await pool.query(
-            'INSERT INTO authors (nombre, email, bio) VALUES ($1, $2, $3) RETURNING *',
-            [nombre, email, bio || null]
-        );
-        res.status(201).json(result.rows[0]);
+        const  author = await createAuthor (nombre, email, bio);
+        res.status(201).json(author);
     }catch (error) {
         if (error.code === '23505') {
             return res.status(409).json({error: 'El email ya esta registrado'});
@@ -49,14 +44,11 @@ router.post('/', async (req, res)=>{
 router.put('/:id', async (req, res)=>{
     const { nombre, email, bio } = req.body;
     try {
-        const result = await pool.query(
-            `UPDATE authors SET nombre = COALESCE($1, nombre), email = COALESCE($2, email),
-            bio = COALESCE($3, bio) WHERE id = $4 RETURNING *`, [nombre, email, bio, req.params.id]
-        );
-        if (result.rows.length === 0) {
+        const author = await updateAuthor( nombre, email, bio, req.params.id);
+        if (!author) {
             return res.status(404).json({ error: 'Autor no encontrado'});
         }
-        res.json(result.rows[0]);
+        res.json(author);
     }catch (error) {
         if (error.code === '23505'){
             return res.status(409).json({error: 'el email ya esta registrado'});
@@ -67,10 +59,8 @@ router.put('/:id', async (req, res)=>{
 
 router.delete('/:id', async (req, res)=>{
     try{
-        const result = await pool.query('DELETE FROM authors WHERE id = $1',
-            [req.params.id]
-        );
-        if (result.rowCount === 0){
+        const authorDeleted = await deleteAuthor(req.params.id)
+        if (!authorDeleted){
             return res.status(404).json({ error: 'Autor no encontrado'});
         }
         res.json({message: 'autor eliminado exitosamente'});
